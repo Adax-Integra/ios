998a34a5052e66dffa7fb55f27e3ae6fbcc8005d
armando f <armandofu0705@gmail.com>
2026-09-24 22:12:41 -0600

    feat(g-02): Added post method with Alamofire

--- ADAXIntegra/Data/APINetwork/APIProtocol.swift
+++ ADAXIntegra/Data/APINetwork/APIProtocol.swift
@@ -8,24 +8,49 @@ import Alamofire
 import Foundation
 
 enum APIError: Error {
-    case requestFailed(String)
+  case requestFailed(String)
 }
 
-
 struct APIProtocol {
-    static func get<T: Decodable>(_ path: String, as type: T.Type) async throws -> T {
-        
-        return try await withCheckedThrowingContinuation { continuation in
-            AF.request(APIConfig.baseURL + path)
-                .validate()
-                .responseDecodable(of: T.self) { response in
-                    switch response.result {
-                    case .success(let value):
-                        continuation.resume(returning: value)
-                    case .failure(let error):
-                        continuation.resume(throwing: APIError.requestFailed(error.localizedDescription))
-                    }
-                }
+  static func get<T: Decodable>(
+    _ path: String,
+    as type: T.Type
+  ) async throws -> T {
+    return try await withCheckedThrowingContinuation { continuation in
+      AF.request(APIConfig.baseURL + path)
+        .validate()
+        .responseDecodable(of: T.self) { response in
+          switch response.result {
+          case .success(let value):
+            continuation.resume(returning: value)
+          case .failure(let error):
+            continuation.resume(throwing: APIError.requestFailed(error.localizedDescription))
+          }
         }
     }
+  }
+
+  static func post<Body: Encodable, T: Decodable>(
+    _ path: String,
+    body: Body,
+    as type: T.Type
+  ) async throws -> T {
+    return try await withCheckedThrowingContinuation { continuation in
+      AF.request(
+        APIConfig.baseURL + path,
+        method: .post,
+        parameters: body,
+        encoder: JSONParameterEncoder.default
+      )
+      .validate()
+      .responseDecodable(of: T.self) { response in
+        switch response.result {
+        case .success(let value):
+          continuation.resume(returning: value)
+        case .failure(let error):
+          continuation.resume(throwing: APIError.requestFailed(error.localizedDescription))
+        }
+      }
+    }
+  }
 }
